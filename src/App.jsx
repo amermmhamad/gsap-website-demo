60e8f979532329197968e565a767deeb7b1d771c
Amer <amermmhamad@gmail.com>
2025-10-22 02:17:51 +0300

    Added About section and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
+      <Features />
     </main>
   );
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,14 +4,15 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
   return (
     <button
       id={id}
-      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass} transition hover:scale-120`}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass} transition-transform group-hover:scale-110`}
     >
-      {leftIcon}
+      <span className="absolute h-0 w-0 rounded-full bg-blue-200 transition-all duration-300 group-hover:h-56 group-hover:w-full hover:border-black group-hover:scale-110"></span>
 
-      <span className="relative inline-flex overlow-hidden font-general text-xs uppercase">
+      <span className="relative inline-flex items-center gap-1 font-general text-xs uppercase group-hover:text-white">
+        {leftIcon}
         <div>{title}</div>
+        {rightIcon}
       </span>
-      {rightIcon}
     </button>
   );
 };
